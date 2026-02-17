beb674eb022bc7b54898fa912e649d9d738f7262
BryanEncalada <93891074+BryanEncalada@users.noreply.github.com>
2026-02-17 18:37:52 -0500

    table

--- src/app/pages/receta/insumos/insumos-lista/insumos-lista.component.ts
+++ src/app/pages/receta/insumos/insumos-lista/insumos-lista.component.ts
@@ -1,15 +1,19 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { TagModule, Tag } from 'primeng/tag';
 import { Insumo } from '../../../../core/models/insumo.model';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-insumos-lista',
-  imports: [],
+  imports: [TableModule, Tag],
   templateUrl: './insumos-lista.component.html',
   styleUrl: './insumos-lista.component.scss',
 })
 export class InsumosListaComponent {
   @Input()
-  insumo: Insumo[] = [];
+  insumos: Insumo[] = [];
 
   @Output() edit = new EventEmitter<Insumo>();
 
