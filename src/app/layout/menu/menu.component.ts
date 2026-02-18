7a1954b2eade00f7549cd3fa4758217cf5aa4fef
Franco Encalada <FrancoEncalada120@gmail.com>
2026-02-18 10:16:47 -0500

    Menu

--- src/app/layout/menu/menu.component.ts
+++ src/app/layout/menu/menu.component.ts
@@ -5,7 +5,7 @@ import {
   RouterLink,
   RouterLinkActive,
 } from '@angular/router';
-import { MenuItem } from './menu.model';
+import { MenuItem, MenuLinkItem } from './menu.model';
 import { filter } from 'rxjs';
 import { NgClass, NgFor, NgIf } from '@angular/common';
 import { ToastModule } from 'primeng/toast';
@@ -31,60 +31,9 @@ import { User } from '../../core/models/user.models';
   styleUrl: './menu.component.scss',
 })
 export class MenuComponent {
-  items: MenuItem[] = [
-    { type: 'section', label: 'OPERATIONS' },
-    { type: 'link', label: 'Dashboard', icon: 'bi-grid', route: 'dashboard' },
-    {
-      type: 'link',
-      label: 'Daily Input',
-      icon: 'bi-plus-circle',
-      route: 'daily-input',
-    },
-    // {
-    //   type: 'link',
-    //   label: 'Monthly P&L',
-    //   icon: 'bi-calendar',
-    //   route: 'monthly-pl',
-    // },
-    {
-      type: 'link',
-      label: 'Profit Calculator',
-      icon: 'bi-calculator',
-      route: 'calculator',
-    },
-    {
-      type: 'link',
-      label: 'Invoice Tracker',
-      icon: 'bi-receipt',
-      route: 'invoice-tracker',
-    },
-    {
-      type: 'group',
-      label: 'Settings',
-      icon: 'bi-gear',
-      isOpen: false,
-      children: [
-        {
-          type: 'link',
-          label: 'Profile',
-          icon: 'bi-person',
-          route: 'settings/Profile',
-        },
-        {
-          type: 'link',
-          label: 'Locations',
-          icon: 'bi-building',
-          route: 'settings/Locacions',
-        },
-        {
-          type: 'link',
-          label: 'Goals',
-          icon: 'bi-building',
-          route: 'settings/Goals',
-        },
-      ],
-    },
-  ];
+
+  items: MenuItem[] = [];
+
 
   showChangePasswordModal = false;
 
@@ -92,6 +41,9 @@ export class MenuComponent {
     private router: Router,
     private authService: UserService,
   ) {
+
+    this.buildMenu();
+
     // Auto-abre el group Settings si estás dentro de /settings/...
     this.router.events
       .pipe(filter((e) => e instanceof NavigationEnd))
@@ -101,6 +53,62 @@ export class MenuComponent {
     console.log('constructor', 'constructor Menu');
 
   }
+
+  buildMenu() {
+    this.items = [
+      { type: 'section', label: 'OPERATIONS' },
+      { type: 'link', label: 'Dashboard', icon: 'bi-grid', route: 'dashboard' },
+      {
+        type: 'link',
+        label: 'Daily Input',
+        icon: 'bi-plus-circle',
+        route: 'daily-input',
+      },
+      {
+        type: 'link',
+        label: 'Profit Calculator',
+        icon: 'bi-calculator',
+        route: 'calculator',
+      },
+      {
+        type: 'link',
+        label: 'Invoice Tracker',
+        icon: 'bi-receipt',
+        route: 'invoice-tracker',
+      },
+      {
+        type: 'group',
+        label: 'Settings',
+        icon: 'bi-gear',
+        isOpen: false,
+        children: [
+          ...(this.userRole === 2
+            ? [
+              {
+                type: 'link',
+                label: 'Profile',
+                icon: 'bi-person',
+                route: 'settings/Profile',
+              } satisfies MenuLinkItem,
+            ]
+            : []),
+          {
+            type: 'link',
+            label: 'Locations',
+            icon: 'bi-building',
+            route: 'settings/Locacions',
+          },
+          {
+            type: 'link',
+            label: 'Goals',
+            icon: 'bi-building',
+            route: 'settings/Goals',
+          },
+        ],
+      },
+    ];
+  }
+
 
   toggleGroup(item: MenuItem) {
     if (item.type !== 'group') return;
@@ -134,9 +142,11 @@ export class MenuComponent {
   }
 
   get User(): User | null {
-
     return this.authService.getUser();
+  }
 
+  get userRole(): number {
+    return this.User?.user_rol ?? 0;
   }
 
   openChangePassword() {
